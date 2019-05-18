de83201ba89d8b7349f7e6f4fabe7717fb8eeb48
Mickey1299 <807798223@qq.com>
2019-05-19 00:19:17 +0800

    将mapper也改成了string，但是用Junit进行测试的时候还是有问题，需要查找SpringMVC怎么对业务层、控制层进行测试

--- src/test/java/team/softwarede/confersys/bizImpl/UserBizImplTest.java
+++ src/test/java/team/softwarede/confersys/bizImpl/UserBizImplTest.java
@@ -6,19 +6,35 @@ package team.softwarede.confersys.bizImpl;
 
 import static org.junit.Assert.*;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import team.softwarede.confersys.biz.UserBiz;
+import team.softwarede.confersys.entity.User;
 
 /**
  * @author Mity1299
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class) 
+@SpringApplicationConfiguration(classes = MockServletContext.class) 
+@WebAppConfiguration 
 public class UserBizImplTest {
+    
+    private MockMvc mvc; 
 
-    @Autowired
-    UserBiz userBiz;
+    @Before(value = "")
+    public void setUp() throws Exception{
+        mvc = MockMvcBuilders.standaloneSetup(new UserBizImpl()).build(); 
+    }
     
     @Test
     public void test() {
@@ -27,8 +43,10 @@ public class UserBizImplTest {
     
     @Test
     public void testLogin() {
-        
-        assertEquals("ok", userBiz.login(1, 1, "123"));
+
+        String msg =userBiz.login("1", "1","123");
+        System.out.println(msg);
+//        assertEquals("ok",msg);
     }
 
 }
---
+++ src/test/java/team/softwarede/confersys/bizImpl/UserBizImplTest2.java
@@ -0,0 +1,22 @@
+/**
+* 
+* @author Mity1299
+*/
+package team.softwarede.confersys.bizImpl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Mity1299
+ *
+ */
+public class UserBizImplTest2 extends UserBizImpl {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
