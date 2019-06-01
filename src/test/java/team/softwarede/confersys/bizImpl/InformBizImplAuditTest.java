7f9ec7cfbe27a585be1e3cb9b4cfda94fe62d313
Mickey1299 <807798223@qq.com>
2019-06-02 03:15:33 +0800

    ”审核会议室申请“功能通过测试

--- src/test/java/team/softwarede/confersys/bizImpl/InformBizImplAuditTest.java
+++ src/test/java/team/softwarede/confersys/bizImpl/InformBizImplAuditTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -32,6 +33,7 @@ public class InformBizImplAuditTest {
     /**
      * 拒绝了会议预约,测试通过
      */
+    @Rollback
     @Test
     public void testSuccess() {
         Integer mtRoomApplyId =2;
@@ -44,6 +46,7 @@ public class InformBizImplAuditTest {
     /**
      * 通过了会议预约，测试通过
      */
+    @Rollback
     @Test
     public void testFail() {
         Integer mtRoomApplyId =2;
