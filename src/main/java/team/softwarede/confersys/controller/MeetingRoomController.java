b36ecef501e127d4069bd8e89320a0f91075e87c
wyf <wyfcat@126.com>
2019-06-12 19:07:10 +0800

    修改一个预约会议室的bug

--- src/main/java/team/softwarede/confersys/controller/MeetingRoomController.java
+++ src/main/java/team/softwarede/confersys/controller/MeetingRoomController.java
@@ -1,5 +1,6 @@
 package team.softwarede.confersys.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -14,8 +15,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import team.softwarede.confersys.biz.MeetingRoomBiz;
 import team.softwarede.confersys.biz.UserGroupBiz;
 import team.softwarede.confersys.dto.BasicSession;
+import team.softwarede.confersys.dto.MeetingRoomAvail;
 import team.softwarede.confersys.dto.MeetingRoomShowAvail;
 import team.softwarede.confersys.dto.UserAndGroup;
+import team.softwarede.confersys.dto.UserLogin;
 import team.softwarede.confersys.entity.EquipmentType;
 import team.softwarede.confersys.vo.UserAndGroupList;
 
@@ -33,12 +36,16 @@ public class MeetingRoomController {
 	public String book(ModelMap map,
 			@ModelAttribute("mtRoomAvail") MeetingRoomShowAvail mtRoomAvail,
 			@ModelAttribute("uAndGList") UserAndGroupList uAndGList,
+			@ModelAttribute("selectedUAndGList") UserAndGroupList selectedUAndGList,
+			RedirectAttributes attributes, 
 			HttpSession session) {
 		BasicSession userSession = (BasicSession) session.getAttribute("userSession");
 		map.addAttribute("userSession",userSession);
 		
 		List<EquipmentType> equipmentTypeList = meetingRoomBiz.showMtRoomBookPage();
 		map.addAttribute("equipmentTypeList",equipmentTypeList);
+		
+		map.addAttribute("selectedUAndGList", (UserAndGroupList)session.getAttribute("selectedUAndGList"));
 		
 		return "mtRoom_searchAvail";
 	}
@@ -48,27 +55,62 @@ public class MeetingRoomController {
 			@ModelAttribute("mtRoomSearch") String mtRoomSearch,
 			RedirectAttributes attributes, 
 			HttpSession session) {
-//		System.out.println("输入："+mtRoomSearch);
+		
 		List<UserAndGroup> ListuAndG = userGroupBiz.searchUAndUGByKeyword(mtRoomSearch, ((BasicSession) session.getAttribute("userSession")).getUserId());
 		UserAndGroupList uAndGList = new UserAndGroupList();
 		uAndGList.setuAndGList(ListuAndG);
 		attributes.addFlashAttribute("uAndGList",uAndGList);
+		
 		return "redirect:/meetingRoom/book.do";
 	}
 	
-//	@RequestMapping("/addUAndUG.do")
-//	public String searchAvail(ModelMap map,
-//			@ModelAttribute("uAndG") UserAndGroup uAndG,
-//			RedirectAttributes attributes, 
-//			HttpSession session) {
-//		System.out.println("输入："+mtRoomSearch);
-//		List<UserAndGroup> ListuAndG = userGroupBiz.searchUAndUGByKeyword(mtRoomSearch, ((BasicSession) session.getAttribute("userSession")).getUserId());
-//		UserAndGroupList uAndGList = new UserAndGroupList();
-//		uAndGList.setuAndGList(ListuAndG);
-//		attributes.addFlashAttribute("uAndGList",uAndGList);
-//		return "redirect:/meetingRoom/book.do";
-//	}
-//	@RequestMapping("/tobook.do")
+	@RequestMapping(value = "/addUAndUG.do", params = { "uAndGId", "uAndGName", "uAndGType"})
+	public String searchAvail(ModelMap map,
+			@ModelAttribute("uAndGId") String uAndGId,
+			@ModelAttribute("uAndGName") String uAndGName,
+			@ModelAttribute("uAndGType") Integer uAndGType,
+			RedirectAttributes attributes, 
+			HttpSession session) {
+		UserAndGroupList selectedUAndGList = (UserAndGroupList)session.getAttribute("selectedUAndGList");
+		UserAndGroup uAndG = new UserAndGroup();
+		uAndG.setId(uAndGId);
+		uAndG.setName(uAndGName);
+		uAndG.setType(uAndGType);
+		List<UserAndGroup> newList = new ArrayList<UserAndGroup>();
+		if (selectedUAndGList != null && selectedUAndGList.getuAndGList()!=null) {
+			if (selectedUAndGList.getuAndGList().size() != 0) {
+				for (UserAndGroup tempUAndG : selectedUAndGList.getuAndGList()) {
+					newList.add(tempUAndG);
+				}
+			}
+		}
+		newList.add(uAndG);
+		System.out.println("uAndG.Type="+uAndG.getType());	
+		System.out.println("uAndG.Name="+uAndG.getName());
+		UserAndGroupList newSelectedUAndGList = new UserAndGroupList();
+		newSelectedUAndGList.setuAndGList(newList);
+		
+		session.setAttribute("selectedUAndGList", newSelectedUAndGList);
+		
+		return "redirect:/meetingRoom/book.do";
+	}
+	
+	@RequestMapping("/tobook.do")
+	public String tobook(ModelMap map,
+			@ModelAttribute("mtRoomAvail") MeetingRoomShowAvail mtRoomAvail,
+			HttpSession session) {
+		BasicSession userSession = (BasicSession) session.getAttribute("userSession");
+		map.addAttribute("userSession",userSession);
+		
+		UserAndGroupList selectedUAndGList = (UserAndGroupList)session.getAttribute("selectedUAndGList");
+		
+		List<MeetingRoomAvail> allMtRoomAvail = meetingRoomBiz.showAvailMtRoom
+				(mtRoomAvail.getBeginTime(), mtRoomAvail.getEndTime(), mtRoomAvail.getEquipmentTypeIdList(), selectedUAndGList.getuAndGList().size());
+		
+		map.addAttribute("allMtRoomAvail", allMtRoomAvail);
+		
+		return "mtRoom_book";
+	}
 	
 	
 }
